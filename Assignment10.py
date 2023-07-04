50abac3151e2737edc7431218d5cf2e56a76040f
Parth <137251828+rabbitb8mile@users.noreply.github.com>
2023-07-04 21:20:06 +0530

    Add files via upload

---
+++ Assignment10.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+import webbrowser as wb
+
+obj=tk.Tk(className="Reddit")
+
+e=tk.Entry(obj,
+           font=("Copper Black",25),
+           width=50)
+e.grid()
+
+def display():
+    s=e.get()
+    print(s)
+    wb.open("https://www.reddit.com/search?q="+s)
+    print("you ar enavigating to Reddit search bar")
+    
+
+b=tk.Button(obj,text="Button",
+            font=("Copper Black",25),
+          
+            activebackground="grey",command=display)
+
+b.grid(row=0,column=1)
+obj.mainloop()
